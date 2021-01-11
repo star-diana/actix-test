21cbe97ffc60b450224be827898b2f35e8376544
FETE <starshine@aili.moe>
2021-01-11 10:37:22 +0800

    feat: 更换日志记录模块

--- src/config/log.rs
+++ src/config/log.rs
@@ -1,4 +1,4 @@
-extern crate env_logger;
+extern crate log4rs;
 
 use std::env;
 
@@ -7,5 +7,5 @@ use dotenv;
 pub fn init_logger() {
     env::set_var("RUST_LOG", dotenv::var("RUST_LOG").unwrap());
     env::set_var("RUST_BACKTRACE", "1");
-    env_logger::init();
+    log4rs::init_file("log.yaml",Default::default()).unwrap();
 }

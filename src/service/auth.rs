bc0d256433c3791bdfd77554d6279eb3af53072e
FETE <starshine@aili.moe>
2021-01-15 21:57:53 +0800

    update: 设置 Cookie

--- src/service/auth.rs
+++ src/service/auth.rs
@@ -1,5 +1,6 @@
 use actix_web::{Error, HttpResponse, post};
 use actix_web::web::Json;
+use actix_web::cookie::Cookie;
 use rbatis::crud::CRUD;
 
 use crate::util::utils::sign_token;
@@ -12,17 +13,16 @@ pub async fn login(lc: Json<LoginCredentials>) -> Result<HttpResponse, Error> {
     let wrapper = RB.new_wrapper().eq("uname", lc.uname.as_str()).check().unwrap();
     let user = RB.fetch_by_wrapper::<Option<User>>("", &wrapper)
         .await
-        .map_err(|e| CustomError::InternalError { message: e.to_string() })?;
+        .map_err(|e| CustomError::InternalError { message: e.to_string() })?
+        .ok_or(CustomError::LoginError { message: "用户名或密码错误".to_string() })?;
+    // 用户名或密码不匹配
+    if user.uname != lc.uname || user.password != Some(lc.password.to_owned()) {
+        return Err(CustomError::LoginError { message: "用户名或密码错误".to_string() }.into());
+    }
 
-    let user = match user {
-        Some(u) => u,
-        None => { return Err(CustomError::LoginError { message: "用户名或密码错误".to_string() }.into()); }
-    };
+    let token = sign_token(user.to_owned())?;
 
-    if !user.uname.eq(&lc.uname) || !user.password.contains(&lc.password) {}
-
-    let token = sign_token(user)?;
-    Ok(HttpResponse::Ok().body(token))
+    Ok(HttpResponse::Ok().cookie(Cookie::new("AILI.MOE", token)).finish())
 }
 
 // pub async fn sign_up() {}

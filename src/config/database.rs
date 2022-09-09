011dba46166076a43222f85b7b6835693eda7122
星光 <starshine@aili.moe>
2022-09-09 17:45:39 +0800

    feat: 使用条件编译，release 编译时从环境变量中读取 DB_URL

--- src/config/database.rs
+++ src/config/database.rs
@@ -3,10 +3,20 @@ use rbdc_mysql::driver::MysqlDriver;
 use crate::config::CONFIG;
 
 pub fn init_pool() -> Rbatis {
-    println!("rbatis pool init ({})...", CONFIG.DB_URL);
+    #[cfg(debug_assertions)]
+    let db_url = {
+        println!("rbatis pool init ({})...", CONFIG.DB_URL);
+        CONFIG.DB_URL.as_str()
+    };
+    
+    #[cfg(not(debug_assertions))]
+    let db_url = {
+        env!("ACTIX_DB_URL", "Environment variable \"ACTIX_DB_URL\" not found!")
+    };
+
     let rbatis = Rbatis::new();
     rbatis
-        .init(MysqlDriver {}, CONFIG.DB_URL.as_str())
+        .init(MysqlDriver {}, db_url)
         .expect("rbatis pool init fail!");
 
     rbatis

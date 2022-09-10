5d49ecc2d23ff173215cb22ea513fd07b8e9ac8b
星光 <starshine@aili.moe>
2022-09-10 18:10:21 +0800

    perf: 使用 pub use 重新导出

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,11 @@
-#[macro_use]
-extern crate rbatis;
-#[macro_use]
-extern crate log;
-
 pub mod config;
 pub mod service;
 pub mod model;
 pub mod middleware;
 pub mod util;
+
+pub use config::application::ApplicationState;
+pub use config::database;
+pub use config::log as app_log;
+pub use config::router;
+pub use config::CONFIG;
--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,15 @@ use actix_web::{
     web, App, HttpServer,
 };
 use std::io;
-use actix_test::config::{application, database, log as app_log, router, CONFIG};
 use log::info;
+use actix_test::{database, app_log, router, ApplicationState, CONFIG};
 
 #[actix_web::main]
 async fn main() -> io::Result<()> {
     // 日志初始化
     app_log::init_logger();
     // app 状态初始化
-    let data = web::Data::new(application::ApplicationState {
+    let data = web::Data::new(ApplicationState {
         rbatis: database::init_pool(),
     });
 
@@ -35,14 +35,14 @@ async fn main() -> io::Result<()> {
             // })
             // 日志中间件
             .wrap(Logger::default())
-            // CORS 中间件
-            .wrap(Cors::permissive())
             // 默认响应的头部的中间件
             .wrap(DefaultHeaders::new().add(("X-Server-Version", "0.1")))
+            // CORS 中间件
+            .wrap(Cors::permissive())
             // 配置路由
             .configure(router::router)
     })
-    .bind(format!("{}:{}", CONFIG.BIND_HOST, CONFIG.PORT))?
-    .run()
-    .await
+        .bind(format!("{}:{}", CONFIG.BIND_HOST, CONFIG.PORT))?
+        .run()
+        .await
 }
